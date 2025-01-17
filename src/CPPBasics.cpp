54075ae95d4e61103c4fb885b9d76b8e4b75a237
Pierce13 <piercescott@weber.edu>
2025-01-16 18:55:27 -0700

    finished!

--- src/CPPBasics.cpp
+++ src/CPPBasics.cpp
@@ -2,32 +2,33 @@
 #include <iomanip>
 #include <cmath>
 
-using namespace std;
-
 double incomeCalc(){
     //TODO: Declare a variable of type int called year and set it equal to 2020
     int year = 2020;
+    double moneyBag = 0;
+    double income = 0;
+    int cnt = 0;
     //TODO: write a loop (either for or while will work) that will iterate from 2020-2024
-    //TODO: Inside of the loop, write a cout statement that prompts the user to enter their
-    //      their income for the current year. It should say the following:
-    //      "Enter income for <year>:"
-    //TODO: Still within the loop, write a cin statement that prompts the user to enter their
-    //      income (hint: the variable should be of type double)
-    double income;
-    double totalIncome;
     for(int i = 0; i < 5; i++){
-        cout << "Enter income for " << year <<": ";
-        cin >> income;
-        totalIncome += income;
-        year += 1;
-    }s
+        //TODO: Inside of the loop, write a cout statement that prompts the user to enter their
+        //      their income for the current year. It should say the following:
+        //      "Enter income for <year>:"
+        std::cout << "Enter income for " << year << ": " << std::endl;
+        //TODO: Still within the loop, write a cin statement that prompts the user to enter their
+        //      income (hint: the variable should be of type double)
+        std::cin >> income;
+        moneyBag += income;
+        year++;
+        cnt++;
+    }
     //TODO: Calculate the average income (hint: you will need a variable inside the loop
     //      that keeps a running total of the income)
     //      remember that the average is the sum divided by the number of observations
-    double average = totalIncome / 5;
-    //TODO: print the average to the console and format the output to 2 decimal places
-    cout << fixed << setprecision(2) << average << endl;
+    double average = moneyBag / cnt;
 
+    //TODO: print the average to the console and format the output to 2 decimal places
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << "Your average income is: " << average << std::endl;
     return average;
 }
 
